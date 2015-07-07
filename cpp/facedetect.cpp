09b8d055b143016d78697bc41d1be936e0f169ff
Ry0 <k104037r@mail.kyutech.jp>
2015-07-07 23:19:53 +0900

    [Fix] cppのディレクトリ移動

--- cpp/facedetect.cpp
+++ cpp/facedetect.cpp
@@ -6,273 +6,255 @@
 #include <iostream>
 #include <iterator>
 #include <stdio.h>
+#include <boost/filesystem.hpp> // ファイル一覧を取得
+#include <boost/foreach.hpp>
+#include <sys/stat.h> // ディレクトリ・ファイル名の確認
+#include <sys/types.h> // ディレクトリ作成
 
 using namespace std;
 using namespace cv;
+using namespace boost;
+
+namespace fs = boost::filesystem;
 
 static void help()
 {
-    cout << "\nThis program demonstrates the cascade recognizer. Now you can use Haar or LBP features.\n"
-            "This classifier can recognize many kinds of rigid objects, once the appropriate classifier is trained.\n"
-            "It's most known use is for faces.\n"
-            "Usage:\n"
-            "./facedetect [--cascade=<cascade_path> this is the primary trained classifier such as frontal face]\n"
-               "   [--nested-cascade[=nested_cascade_path this an optional secondary classifier such as eyes]]\n"
-               "   [--scale=<image scale greater or equal to 1, try 1.3 for example>]\n"
-               "   [--try-flip]\n"
-               "   [filename|camera_index]\n\n"
-            "see facedetect.cmd for one call:\n"
-            "./facedetect --cascade=\"../../data/haarcascades/haarcascade_frontalface_alt.xml\" --nested-cascade=\"../../data/haarcascades/haarcascade_eye.xml\" --scale=1.3\n\n"
+  cout <<   "./facedetect --input=\"directory\" --output=\"directory\"\n\n"
             "During execution:\n\tHit any key to quit.\n"
             "\tUsing OpenCV version " << CV_VERSION << "\n" << endl;
 }
 
-void detectAndDraw( Mat& img, CascadeClassifier& cascade,
-                    CascadeClassifier& nestedCascade,
-                    double scale, bool tryflip );
+void detectAndSave(CascadeClassifier &cascade, string &srcfilename, string &outputpath);
+string ExtractFileName(const string &path, bool without_extension);
+bool isInImage(cv::Mat &img, cv::Rect &rect);
+cv::Rect fixRectSize(cv::Mat &img, cv::Rect &rect);
 
-string cascadeName = "../../data/haarcascades/haarcascade_frontalface_alt.xml";
-string nestedCascadeName = "../../data/haarcascades/haarcascade_eye_tree_eyeglasses.xml";
+string cascadeName = "/usr/local/share/OpenCV/haarcascades/haarcascade_frontalface_alt.xml";
+
+// class Argument
+// {
+// public:
+//   Argument(string name)
+// 	:name_(name), prefix_name_(name), prefix_name_len_(name.length()), flag_(false){}
+//   ~Argument(){}
+//   int prefix_compare(char* argv){
+// 	int result = prefix_name_.compare(0, prefix_name_len_, argv, prefix_name_len_);
+// 	if(result == 0) { flag_ = true; }
+// 	return result
+//   }
+//   void rm_prefix(){
+// 	name_.assign(prefix_name_.c_str() + prefix_name_len_);
+//   }
+// private:
+//   string prefix_name_;
+//   stirng prefix_name_len_;
+//   stirng name_;
+//   bool flag_;
+// };
+
+
+// class ArgumentParser
+// {
+// public:
+//   ArgumentParser(std::vector<string> options):
+// 	inputDir_(options[0]), outputDir_(options[1]){}
+//   ~ArgumentParser(){}
+//   perse(int argc, const char** argv){
+// 	for( int i = 1; i < argc; i++ ){
+// 	  if( inputDir_.prefix_compare( argv[i] ) == 0){
+// 		inputDir_.rm_prefix();
+// 	  }
+// 	  else if( outputDir_.prefix_compare( argv[i] ) == 0){
+// 		outputDir_.rm_prefix();
+// 	  }else{
+// 		cerr << "WARNING: Unknown option %s" << argv[i] << endl;
+// 	  }
+//     }
+//   }
+// private:
+//   Argument inputDir_;
+//   Argument outputDir_;
+// };
 
 int main( int argc, const char** argv )
 {
-    CvCapture* capture = 0;
-    Mat frame, frameCopy, image;
-    const string scaleOpt = "--scale=";
-    size_t scaleOptLen = scaleOpt.length();
-    const string cascadeOpt = "--cascade=";
-    size_t cascadeOptLen = cascadeOpt.length();
-    const string nestedCascadeOpt = "--nested-cascade";
-    size_t nestedCascadeOptLen = nestedCascadeOpt.length();
-    const string tryFlipOpt = "--try-flip";
-    size_t tryFlipOptLen = tryFlipOpt.length();
-    string inputName;
-    bool tryflip = false;
+  Mat frame, frameCopy, image;
+  string inputDir = "--input=";
+  size_t inputDirLen = inputDir.length();
+  bool isInputDir = false;
+  string outputDir = "--output=";
+  size_t outputDirLen = outputDir.length();
+  bool isOutputDir = false;
+  CascadeClassifier cascade;
+  string inputName;
 
-    help();
+  help();
 
-    CascadeClassifier cascade, nestedCascade;
-    double scale = 1;
-
-    for( int i = 1; i < argc; i++ )
+  // 引数処理
+  for( int i = 1; i < argc; i++ )
     {
-        cout << "Processing " << i << " " <<  argv[i] << endl;
-        if( cascadeOpt.compare( 0, cascadeOptLen, argv[i], cascadeOptLen ) == 0 )
+	  cout << "Processing " << i << " " <<  argv[i] << endl;
+	  if( inputDir.compare( 0, inputDirLen, argv[i], inputDirLen) == 0)
+		{
+		  inputDir.assign( argv[i] + inputDirLen);
+		  cout << "input directory = " << inputDir << endl;
+		  isInputDir = true;
+		}
+	  else if( outputDir.compare( 0, outputDirLen, argv[i], outputDirLen) == 0)
+		{
+		  outputDir.assign( argv[i] + outputDirLen);
+		  cout << "output directory = " << outputDir << endl;
+		  isOutputDir = true;
+		}
+	  else if( argv[i][0] == '-' )
         {
-            cascadeName.assign( argv[i] + cascadeOptLen );
-            cout << "  from which we have cascadeName= " << cascadeName << endl;
+		  cerr << "WARNING: Unknown option %s" << argv[i] << endl;
         }
-        else if( nestedCascadeOpt.compare( 0, nestedCascadeOptLen, argv[i], nestedCascadeOptLen ) == 0 )
-        {
-            if( argv[i][nestedCascadeOpt.length()] == '=' )
-                nestedCascadeName.assign( argv[i] + nestedCascadeOpt.length() + 1 );
-            if( !nestedCascade.load( nestedCascadeName ) )
-                cerr << "WARNING: Could not load classifier cascade for nested objects" << endl;
-        }
-        else if( scaleOpt.compare( 0, scaleOptLen, argv[i], scaleOptLen ) == 0 )
-        {
-            if( !sscanf( argv[i] + scaleOpt.length(), "%lf", &scale ) || scale < 1 )
-                scale = 1;
-            cout << " from which we read scale = " << scale << endl;
-        }
-        else if( tryFlipOpt.compare( 0, tryFlipOptLen, argv[i], tryFlipOptLen ) == 0 )
-        {
-            tryflip = true;
-            cout << " will try to flip image horizontally to detect assymetric objects\n";
-        }
-        else if( argv[i][0] == '-' )
-        {
-            cerr << "WARNING: Unknown option %s" << argv[i] << endl;
-        }
-        else
-            inputName.assign( argv[i] );
+	  else
+		inputName.assign( argv[i] );
     }
 
-    if( !cascade.load( cascadeName ) )
+  // 入力画像ディレクトリの確認
+  if(! isInputDir)
+	{
+	  cerr << "No input directory... " << endl;
+	  help();
+	  return -1;
+	}
+  else
+	{
+	  struct stat st;
+	  if(stat(inputDir.c_str(), &st) != 0)
+		{
+		  cerr << "No such a directory " << inputDir << endl;
+		  return -1;
+		}
+	}
+
+  // 出力画像ディレクトリの確認
+  if(! isOutputDir)
+	{
+	  cerr << "No output directory" << endl;
+	  cout << "will make a directory \"output\"" << endl;
+	  mkdir("output", 0755); // "outputディレクトリを作成"
+	  outputDir.assign("output" + outputDirLen);
+	}
+  else
+	{
+	  struct stat st;
+	  if(stat(outputDir.c_str(), &st) != 0)
+		{
+		  cerr << "No such a directory " << outputDir << endl;
+		  cout << "will make a directory "  << outputDir << endl;
+		  mkdir(outputDir.c_str(), 0755);
+		}
+	}
+
+  if( !cascade.load( cascadeName ) )
     {
-        cerr << "ERROR: Could not load classifier cascade" << endl;
-        help();
-        return -1;
+	  cerr << "ERROR: Could not load classifier cascade" << endl;
+	  cout << "Please Check a cascade file is exist at " << 
+		"\"" << cascadeName << "\"" << endl;
+	  return -1;
     }
 
-    if( inputName.empty() || (isdigit(inputName.c_str()[0]) && inputName.c_str()[1] == '\0') )
-    {
-        capture = cvCaptureFromCAM( inputName.empty() ? 0 : inputName.c_str()[0] - '0' );
-        int c = inputName.empty() ? 0 : inputName.c_str()[0] - '0' ;
-        if(!capture) cout << "Capture from CAM " <<  c << " didn't work" << endl;
-    }
-    else if( inputName.size() )
-    {
-        image = imread( inputName, 1 );
-        if( image.empty() )
-        {
-            capture = cvCaptureFromAVI( inputName.c_str() );
-            if(!capture) cout << "Capture from AVI didn't work" << endl;
-        }
-    }
-    else
-    {
-        image = imread( "lena.jpg", 1 );
-        if(image.empty()) cout << "Couldn't read lena.jpg" << endl;
-    }
+  //inputディレクトリのファイル一覧を取得する
+  // http://boostjp.github.io/tips/filesystem.html
+  std::vector<std::string> file_list;
+  const fs::path path(inputDir);
+  BOOST_FOREACH(const fs::path& p, std::make_pair(fs::recursive_directory_iterator(path),
+												  fs::recursive_directory_iterator())) {
+	if (!fs::is_directory(p))
+	  file_list.push_back(p.string());
+	std::cout << p.filename() << std::endl;
+  }
+  
+  for(vector<string>::iterator iter = file_list.begin(); iter != file_list.end(); iter++){
+	try{
+	  detectAndSave(cascade, *iter, outputDir);
+	}catch(...){
+	  cerr << "なんか例外" << endl;
+ 	}
+  }
 
-    cvNamedWindow( "result", 1 );
 
-    if( capture )
-    {
-        cout << "In capture ..." << endl;
-        for(;;)
-        {
-            IplImage* iplImg = cvQueryFrame( capture );
-            frame = iplImg;
-            if( frame.empty() )
-                break;
-            if( iplImg->origin == IPL_ORIGIN_TL )
-                frame.copyTo( frameCopy );
-            else
-                flip( frame, frameCopy, 0 );
+  return 0;
 
-            detectAndDraw( frameCopy, cascade, nestedCascade, scale, tryflip );
-
-            if( waitKey( 10 ) >= 0 )
-                goto _cleanup_;
-        }
-
-        waitKey(0);
-
-_cleanup_:
-        cvReleaseCapture( &capture );
-    }
-    else
-    {
-        cout << "In image read" << endl;
-        if( !image.empty() )
-        {
-            detectAndDraw( image, cascade, nestedCascade, scale, tryflip );
-            waitKey(0);
-        }
-        else if( !inputName.empty() )
-        {
-            /* assume it is a text file containing the
-            list of the image filenames to be processed - one per line */
-            FILE* f = fopen( inputName.c_str(), "rt" );
-            if( f )
-            {
-                char buf[1000+1];
-                while( fgets( buf, 1000, f ) )
-                {
-                    int len = (int)strlen(buf), c;
-                    while( len > 0 && isspace(buf[len-1]) )
-                        len--;
-                    buf[len] = '\0';
-                    cout << "file " << buf << endl;
-                    image = imread( buf, 1 );
-                    if( !image.empty() )
-                    {
-                        detectAndDraw( image, cascade, nestedCascade, scale, tryflip );
-                        c = waitKey(0);
-                        if( c == 27 || c == 'q' || c == 'Q' )
-                            break;
-                    }
-                    else
-                    {
-                        cerr << "Aw snap, couldn't read image " << buf << endl;
-                    }
-                }
-                fclose(f);
-            }
-        }
-    }
-
-    cvDestroyWindow("result");
-
-    return 0;
 }
 
-void detectAndDraw( Mat& img, CascadeClassifier& cascade,
-                    CascadeClassifier& nestedCascade,
-                    double scale, bool tryflip )
+// 顔検出と保存
+void detectAndSave(CascadeClassifier &cascade, string &srcfilename, string &outputpath)
 {
-    int i = 0;
-    double t = 0;
-    vector<Rect> faces, faces2;
-    const static Scalar colors[] =  { CV_RGB(0,0,255),
-        CV_RGB(0,128,255),
-        CV_RGB(0,255,255),
-        CV_RGB(0,255,0),
-        CV_RGB(255,128,0),
-        CV_RGB(255,255,0),
-        CV_RGB(255,0,0),
-        CV_RGB(255,0,255)} ;
-    Mat gray, smallImg( cvRound (img.rows/scale), cvRound(img.cols/scale), CV_8UC1 );
+  Mat inputImage = imread(srcfilename, 1);
+  Mat grayImage;
+  cvtColor(inputImage, grayImage,  CV_RGB2GRAY);
+  Mat eqalizedImage = inputImage;
+  // equalizeHist( grayImage, eqalizedImage );
+  vector<Rect> faces;
 
-    cvtColor( img, gray, CV_BGR2GRAY );
-    resize( gray, smallImg, smallImg.size(), 0, 0, INTER_LINEAR );
-    equalizeHist( smallImg, smallImg );
+  cascade.detectMultiScale( eqalizedImage, faces,
+							1.1, 1, 0, Size(50, 50) );
+  // 切り出して保存
+  int num = 1;
+  for (vector<Rect>::iterator iter = faces.begin(); iter != faces.end(); iter ++) {
+	string saveImgname = ExtractFileName(srcfilename, true);
+	string saveImgpath = outputpath + "/" + saveImgname + "_" + to_string(num) + ".jpg";
+	cout << saveImgpath << endl;
+	num++;
+	Rect newRect = Rect(iter->x - (iter->width)*0.3,
+						iter->y - (iter->height)*0.3,
+						(iter->width)*1.6,
+						(iter->height)*1.6);
+   
+	cout << *iter << endl;
+	cout << newRect << endl;
+	Rect fixedRect = fixRectSize(inputImage, newRect);
+	cout << fixedRect << endl;cout << endl;
+	imwrite(saveImgpath, inputImage(fixedRect));
+  }
+}
 
-    t = (double)cvGetTickCount();
-    cascade.detectMultiScale( smallImg, faces,
-        1.1, 2, 0
-        //|CV_HAAR_FIND_BIGGEST_OBJECT
-        //|CV_HAAR_DO_ROUGH_SEARCH
-        |CV_HAAR_SCALE_IMAGE
-        ,
-        Size(30, 30) );
-    if( tryflip )
-    {
-        flip(smallImg, smallImg, 1);
-        cascade.detectMultiScale( smallImg, faces2,
-                                 1.1, 2, 0
-                                 //|CV_HAAR_FIND_BIGGEST_OBJECT
-                                 //|CV_HAAR_DO_ROUGH_SEARCH
-                                 |CV_HAAR_SCALE_IMAGE
-                                 ,
-                                 Size(30, 30) );
-        for( vector<Rect>::const_iterator r = faces2.begin(); r != faces2.end(); r++ )
-        {
-            faces.push_back(Rect(smallImg.cols - r->x - r->width, r->y, r->width, r->height));
-        }
-    }
-    t = (double)cvGetTickCount() - t;
-    printf( "detection time = %g ms\n", t/((double)cvGetTickFrequency()*1000.) );
-    for( vector<Rect>::const_iterator r = faces.begin(); r != faces.end(); r++, i++ )
-    {
-        Mat smallImgROI;
-        vector<Rect> nestedObjects;
-        Point center;
-        Scalar color = colors[i%8];
-        int radius;
+// パスからファイル名を抽出（拡張子を除くフラグ付き）
+// http://www.slis.tsukuba.ac.jp/~fujisawa.makoto.fu/cgi-bin/wiki/index.php?string%A4%CB%A4%E8%A4%EB%CA%B8%BB%FA%CE%F3%BD%E8%CD%FD#w175146e
+string ExtractFileName(const string &path, bool without_extension = true)
+{
+  string fn;
+  string::size_type fpos;
+  if((fpos = path.find_last_of("/")) != string::npos){
+	fn = path.substr(fpos+1);
+  }
+  else if((fpos = path.find_last_of("\\")) != string::npos){
+	fn = path.substr(fpos+1);
+  }
+  else{
+	fn = path;
+  }
+ 
+  if(without_extension && (fpos = fn.find_last_of(".")) != string::npos){
+	fn = fn.substr(0, fpos);
+  }
+ 
+  return fn;
+}
 
-        double aspect_ratio = (double)r->width/r->height;
-        if( 0.75 < aspect_ratio && aspect_ratio < 1.3 )
-        {
-            center.x = cvRound((r->x + r->width*0.5)*scale);
-            center.y = cvRound((r->y + r->height*0.5)*scale);
-            radius = cvRound((r->width + r->height)*0.25*scale);
-            circle( img, center, radius, color, 3, 8, 0 );
-        }
-        else
-            rectangle( img, cvPoint(cvRound(r->x*scale), cvRound(r->y*scale)),
-                       cvPoint(cvRound((r->x + r->width-1)*scale), cvRound((r->y + r->height-1)*scale)),
-                       color, 3, 8, 0);
-        if( nestedCascade.empty() )
-            continue;
-        smallImgROI = smallImg(*r);
-        nestedCascade.detectMultiScale( smallImgROI, nestedObjects,
-            1.1, 2, 0
-            //|CV_HAAR_FIND_BIGGEST_OBJECT
-            //|CV_HAAR_DO_ROUGH_SEARCH
-            //|CV_HAAR_DO_CANNY_PRUNING
-            |CV_HAAR_SCALE_IMAGE
-            ,
-            Size(30, 30) );
-        for( vector<Rect>::const_iterator nr = nestedObjects.begin(); nr != nestedObjects.end(); nr++ )
-        {
-            center.x = cvRound((r->x + nr->x + nr->width*0.5)*scale);
-            center.y = cvRound((r->y + nr->y + nr->height*0.5)*scale);
-            radius = cvRound((nr->width + nr->height)*0.25*scale);
-            circle( img, center, radius, color, 3, 8, 0 );
-        }
-    }
-    cv::imshow( "result", img );
+// Rectの範囲が画像内かどうかチェック
+bool isInImage(cv::Mat &img, cv::Rect &rect)
+{
+  if(rect.x < 0) return false;
+  if(rect.y < 0) return false;
+  if(rect.x + rect.width > img.cols) return false;
+  if(rect.y + rect.height > img.rows) return false;
+  return true;
+}
+
+// Rectの範囲が画像内になければ修正する
+cv::Rect fixRectSize(cv::Mat &img, cv::Rect &rect)
+{
+  if(isInImage(img, rect)) return rect;
+  cv::Rect fixedRect = rect;
+  if(rect.x < 0) fixedRect.x = 0;
+  if(rect.y < 0) fixedRect.y = 0;
+  if(fixedRect.x + rect.width > img.cols) fixedRect.width = img.cols - fixedRect.x;
+  if(fixedRect.y + rect.height > img.rows) fixedRect.height = img.rows - fixedRect.y;
+  return fixedRect;
 }
